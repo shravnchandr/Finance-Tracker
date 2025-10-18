6491cd10a9fc52ec0b2e6ce7de41684ea6a6c06b
Shravan Chandra <52000043+shravnchandr@users.noreply.github.com>
2025-10-19 00:12:10 +0530

    Toggle debug mode for app execution

--- app.py
+++ app.py
@@ -482,5 +482,5 @@ def delete_category(category_id):
     return jsonify({'message': 'Category deleted successfully'})
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=8080)
