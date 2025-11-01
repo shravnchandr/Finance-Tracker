888969dc2b1f3997211fd6dfab882da7f7319831
Shravan Chandra <52000043+shravnchandr@users.noreply.github.com>
2025-11-01 17:46:38 +0530

    Add SSL context to app.run configuration
    Enable SSL for the application by adding ssl_context.

--- app.py
+++ app.py
@@ -487,5 +487,6 @@ def delete_category(category_id):
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False, 
+            ssl_context=('cert.pem', 'key.pem'))
     
